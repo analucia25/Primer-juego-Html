71d9fa12f1d75cc3d2eba665f1037c12c4959f42
analucia25 <echevarriaanalucia@gmail.com>
2025-08-09 01:38:13 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+//Variables 
+let NumeroMaxposible = 100;
+let numeroSecreto = Math.floor(Math.random()*NumeroMaxposible)+1;
+let numeroDeUsuario ;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 6;
+
+console.log(numeroSecreto)
+while(numeroDeUsuario != numeroSecreto){ 
+    numeroDeUsuario = parseInt(prompt(`Me indicas un numero por favor del 1 al ${NumeroMaxposible} : `));
+
+    console.log(typeof(numeroDeUsuario));
+    /*Este codigo realiza la codificacion
+    */
+    if (numeroDeUsuario == numeroSecreto) {
+        //fue verdadera la condicion
+        alert(`Acertaste, el numero es: ${numeroDeUsuario}.Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }.`);
+
+    } else {
+        if(numeroDeUsuario > numeroSecreto){
+            alert('El numero secreto es menor.');
+        } else {
+            alert('El numero secreto es mayor.');
+        }
+        //Incrementamos el contador cuando no acierta
+        intentos++;
+        palabraVeces = 'veces';
+        if (intentos > 6) {
+            alert(`Llegaste al numero maximo de intentos ${maximosIntentos} ${intentos == 1 ? 'vez' : 'veces' }.`);
+            break;
+        }
+        //la condicion no se cumplio 
+        //alert('Lo siento , no acertaste el numero');
+    }
+ }
